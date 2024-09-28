7b0ab43773309a7637096fff395ddbf9a97d8a14
nursugianto1001 <h1101231053@student.untan.ac.id>
2024-09-28 21:00:50 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- sebelas.php
+++ sebelas.php
@@ -6,8 +6,7 @@ use App\Admin\Dosen;
 $dian = New Dosen();
 $dian->nama = 'Dian Prawira';
 $dian->nip = '198411132015041001';
-$dian->getNoHp(62111111);
+$dian->setNoHp(62111111);
 $dian-> alamat = 'Jln purnama';
 $dian->nidn = '1000121184001';
-$dian->mengajar();
 ?>
